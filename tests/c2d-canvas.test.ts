ab19957bbe2939e60cb63907f850d4f711a12bb8
Caleb Foss <calebfoss@gmail.com>
2025-05-29 13:44:35 -0500

    basic c2d-canvas tests

--- tests/c2d-canvas.test.ts
+++ tests/c2d-canvas.test.ts
@@ -1,20 +1,68 @@
 import { expect, jest, test } from "@jest/globals";
 import { setupJestCanvasMock } from "jest-canvas-mock";
-import { createCanvas } from "web-spinner";
+import { Color, createCanvas } from "web-spinner";
 
 beforeEach(() => {
   jest.resetAllMocks();
   setupJestCanvasMock();
 });
 
-test("c2d-canvas", () => {
-  const width = 1;
+const width = 150;
 
-  const height = 2;
+const height = 250;
 
-  const canvas = createCanvas({ width, height });
+function dimensions(element: { width: number; height: number }) {
+  test("width and height initialized", () => {
+    expect(element.width).toBe(width);
 
-  expect(canvas.width).toBe(width);
+    expect(element.height).toBe(height);
+  });
 
-  expect(canvas.height).toBe(height);
+  test("width and height modified", () => {
+    const change = 50;
+
+    element.width += change;
+
+    element.height += change;
+
+    expect(element.width).toBe(width + change);
+
+    expect(element.height).toBe(height + change);
+
+    element.width -= change;
+
+    element.height -= change;
+
+    expect(element.width).toBe(width);
+
+    expect(element.height).toBe(height);
+  });
+}
+
+describe("c2d-canvas", () => {
+  const background = Color.rgb(50, 100, 150);
+
+  const canvas = createCanvas({ width, height, background });
+
+  dimensions(canvas);
+
+  test("background", () => {
+    expect(canvas.background instanceof Color).toBe(true);
+
+    expect(background.equals(canvas.background as Color)).toBe(true);
+  });
+
+  test("center", () => {
+    expect(canvas.center.x).toBe(width / 2);
+
+    expect(canvas.center.y).toBe(height / 2);
+  });
+
+  test("domCanvas", () => {
+    expect(canvas.domCanvas instanceof HTMLCanvasElement).toBe(true);
+  });
+
+  test("context", () => {
+    expect(canvas.context instanceof CanvasRenderingContext2D).toBe(true);
+  });
 });
