6930816029806d1fcad35f49dfe7102a3a5c59d7
Caleb Foss <calebfoss@gmail.com>
2025-06-16 19:29:55 -0500

    Add stroke test

--- tests/stroke.ts
+++ tests/stroke.ts
@@ -2,9 +2,31 @@ import { expect, jest, test } from "@jest/globals";
 import { waitFor } from "@testing-library/dom";
 import { ElementTestSetup, VoidCanvasMethodNames } from "./types";
 import { testReflection } from "./shared";
+import { DrawStyle } from "../dist/types/classes/gradient";
+import {
+  Color,
+  LinearGradient,
+  RadialGradient,
+  ConicalGradient,
+} from "web-spinner";
 
 export function testStroke(
-  setup: ElementTestSetup<{ lineWidth: number | null }>,
+  setup: ElementTestSetup<{
+    lineWidth: number | null;
+    stroke: DrawStyle | null;
+    renderConicalGradient: (
+      context: CanvasRenderingContext2D,
+      gradient: ConicalGradient
+    ) => CanvasGradient;
+    renderLinearGradient: (
+      context: CanvasRenderingContext2D,
+      gradient: LinearGradient
+    ) => CanvasGradient;
+    renderRadialGradient: (
+      context: CanvasRenderingContext2D,
+      gradient: RadialGradient
+    ) => CanvasGradient;
+  }>,
   renderFunctionName: VoidCanvasMethodNames
 ) {
   describe("stroke", () => {
@@ -30,6 +52,65 @@ export function testStroke(
       });
 
       testReflection(element, "lineWidth", "line-width", 7);
+    });
+  });
+
+  test("stroke", async () => {
+    const { canvas, element } = setup();
+
+    let renderedStroke: string | CanvasGradient | CanvasPattern;
+
+    const render = jest.fn(() => {
+      renderedStroke = canvas.context.strokeStyle;
+    });
+
+    canvas.context[renderFunctionName] = render;
+
+    const color = Color.rgb(75, 150, 225);
+
+    element.stroke = color;
+
+    await waitFor(() => {
+      expect(render).toHaveBeenCalled();
+
+      expect(renderedStroke).toBe(color.toString());
+    });
+
+    testReflection(element, "stroke", "stroke", Color.rgb(225, 150, 75));
+
+    const linearGradient = new LinearGradient();
+
+    element.stroke = linearGradient;
+
+    await waitFor(() => {
+      expect(render).toHaveBeenCalledTimes(2);
+
+      expect(renderedStroke instanceof CanvasGradient).toBe(true);
+    });
+
+    const radialGradient = new RadialGradient();
+
+    element.stroke = radialGradient;
+
+    await waitFor(() => {
+      expect(render).toHaveBeenCalledTimes(3);
+
+      expect(renderedStroke instanceof CanvasGradient).toBe(true);
+    });
+
+    const conicalGradient = new ConicalGradient();
+
+    canvas.context.createConicGradient = jest.fn(
+      (...args: Parameters<CanvasRenderingContext2D["createConicGradient"]>) =>
+        new CanvasGradient()
+    );
+
+    element.stroke = conicalGradient;
+
+    await waitFor(() => {
+      expect(render).toHaveBeenCalledTimes(4);
+
+      expect(renderedStroke instanceof CanvasGradient).toBe(true);
     });
   });
 }
