86d56d383df223e22dacc286407c6df11bb68d09
Caleb Foss <calebfoss@gmail.com>
2025-06-16 21:33:19 -0500

    Add angle and scale

--- tests/testTransform.ts
+++ tests/testTransform.ts
@@ -23,5 +23,38 @@ export function testTransform(
         expect(translate.mock.calls[0]).toEqual([anchor.x, anchor.y]);
       });
     });
+
+    test("angle", async () => {
+      const { element, canvas } = setup();
+
+      const rotate = jest.spyOn(canvas.context, "rotate");
+
+      const angle = Angle.degrees(30);
+
+      element.angle = angle;
+
+      await waitFor(() => {
+        expect(rotate).toHaveBeenCalled();
+
+        expect(rotate.mock.calls[0][0]).toBe(angle.radians);
+      });
+    });
+
+    test("scale", async () => {
+      const { element, canvas } = setup();
+
+      const scale = jest.spyOn(canvas.context, "scale");
+
+      const scaleValue = Vector2D.xy(1.5, 2.5);
+
+      element.scale = scaleValue;
+
+      await waitFor(() => {
+        // The first time is for the canvas
+        expect(scale).toHaveBeenCalledTimes(2);
+
+        expect(scale.mock.calls[1]).toEqual([scaleValue.x, scaleValue.y]);
+      });
+    });
   });
 }
